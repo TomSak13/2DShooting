5d84b4c1ee24a79dad432f513cc440f05cccd7f1
TomSak13 <tomohiko.sakaguchi@tatsu-mi.co.jp>
2023-10-17 09:11:22 +0900

    Rendererクラスを追加。ビルドは通る。

--- project/SpriteComponent.cpp
+++ project/SpriteComponent.cpp
@@ -18,6 +18,7 @@ SpriteComponent::SpriteComponent(Actor* owner, int drawOrder)
 	,mDrawOrder(drawOrder)
 	,mTexWidth(0)
 	,mTexHeight(0)
+	, mVisible(true)
 {
 	mOwner->GetGame()->AddSprite(this);
 }
